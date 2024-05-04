2fdabf223d9b124b6df1d20e2e9cf0981d709d00
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-05-04 14:10:42 +0300

    Adds a help flag for cli

--- cli.js
+++ cli.js
@@ -2,12 +2,39 @@
 import * as groups from "./index.js";
 
 const args = process.argv.slice(2);
-const groupName = args[0].replace(/^--/, '');
+const groupName = args[0] ? args[0].replace(/^--/, '') : '';
+const DESCRIPTIONS = {
+  'all': 'Display the All HTML5 tags',
+  'document': 'Document tags',
+  'metadata': 'Metadata tags',
+  'sections': 'Sectioning tags',
+  'grouping': 'Grouping tags',
+  'texts': 'Text tags',
+  'edits': 'Edit tags',
+  'embedded': 'Embedded tags',
+  'tabular': 'Tabular tags',
+  'forms': 'Form tags',
+  'interactive': 'Interactive tags',
+  'scripting': 'Scripting tags',
+  'noEndTags': 'No-end or self-closing tags',
+  'paired': 'Paired tags',
+  'deprecated': 'Tags that are not included in the HTML5 specification, but were in HTML4',
+  'html4': 'All tags are from the HTML4 specification',
+  'html1': 'All tags are from the first HTML specification',
+};
 
-const group = groups[groupName];
-
-if (group) {
-  console.log(group);
+if (groupName === 'help' || args.length === 0) {
+  console.log('Usage: html-tag-warehouse [--option]');
+  console.log('Options:');
+  Object.keys(groups).forEach(key => {
+    console.log(`  --${key}: ${DESCRIPTIONS[key]}`);
+  });
+  console.log('  --help: Display this help message');
 } else {
-  console.log('Unknown command');
+  const group = groups[groupName];
+  if (group) {
+    console.log(group);
+  } else {
+    console.log('Unknown command. Please use --help to see available options.');
+  }
 }
